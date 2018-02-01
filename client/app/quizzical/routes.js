f69c2da32f66df1437b0c1af8e5cfc264fba3680
Imad Collin <Admin@MacBook.local>
2018-02-01 15:50:44 +0100

    Add new quiz page

--- client/app/quizzical/routes.js
+++ client/app/quizzical/routes.js
@@ -5,12 +5,14 @@ import App from './containers/App';
 import HomePage from './containers/HomePage';
 import CounterPage from '../counter/containers/CounterPage';
 import QuizzesPage from '../quizzes/containers/QuizzesPage';
+import AddQuizPage from '../quizzes/containers/AddQuizPage';
 
 export default () => (
   <App>
     <Switch>
       <Route path="/counter" component={CounterPage} />
       <Route path="/quizzes" component={QuizzesPage} />
+      <Route path="/AddQuiz" component={AddQuizPage} />
       <Route path="/" component={HomePage} />
     </Switch>
   </App>
