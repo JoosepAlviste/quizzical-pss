379e261ae692fe98064208c3b964da7578778911
Imad Collin <Admin@student245-238.eduroam.uu.se>
2018-02-08 14:24:34 +0100

    Abswering the questions component

--- client/app/quizzes/components/AnswerQuiz.js
+++ client/app/quizzes/components/AnswerQuiz.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Quiz } from '../reducers/answerQuiz';
+import { Link } from 'react-router-dom';
 
 type Props = {
   quiz?: Quiz,
@@ -9,6 +10,17 @@ type Props = {
 
 class AnswerQuiz extends Component<Props> {
   props: Props;
+  constructor(props) {
+    super(props);
+    this.state = {count: 0};
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange(param) {
+    if (param.correct===true)
+    this.state.count++;
+      console.log(this.state.count)
+}
 
   componentDidMount() {
     this.fetchQuiz();
@@ -20,17 +32,59 @@ class AnswerQuiz extends Component<Props> {
     fetchQuiz(parseInt(match.params.quizId, 10));
   }
 
+
   render() {
     const { quiz } = this.props;
     // Check console or Quiz type for structure
     console.log(quiz);
+    
+    let questionText='No Questions!!'
+    let choice='No choices for this question!!!'
+    
+    if (this.props.quiz.questions) {
+      questionText= quiz.questions.map((ques,i) =>
+      
+      ques.choices.map((f)=>
 
-    return (
-      <h1>Hello world</h1>
-    );
-  }
+      <div>
+          <li>{ques.text}</li>
+
+          <label>
+             
+            Correct :
+              <input
+                name="choice"
+                type="checkbox"
+                // checked={false}
+                // checked={f.correct}
+                // onChange={() => console.log(f.correct)}
+                onChange={() => this.handleChange(f)}
+              />
+            </label>
+            <p>{f.text}</p>
+       </div>
+
+      )
+    );  
+   
 }
 
+    return (
+      <div>
+          <span >Quiz {quiz.id} </span>
+          {quiz.title} 
+          {questionText}
+          toBe Managed as intended:) 
+          <br/>
+          <button onClick={()=>console.log(this.state.count)}>Result</button>
+          {/* <button onClick={()=><div>{console.log(this.state.count)}Result </button> */}
+
+          <Link to="/">Home</Link>
+      </div> 
+    );  
+  
+}
+}
 AnswerQuiz.defaultProps = {
   quiz: null,
 };
