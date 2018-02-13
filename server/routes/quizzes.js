f31a5978a5d7f776cdb16f5e1e000147a746f500
Iosif Kakalelis <joseph13g@gmail.com>
2018-02-13 14:17:42 +0100

    Function done. Joosep helped me finish it by using promises

--- server/routes/quizzes.js
+++ server/routes/quizzes.js
@@ -2,79 +2,95 @@ const express = require("express");
 const Sequelize = require('sequelize');
 
 const routesFunction = (sequelize) => {
-  const router = express.Router();
+    const router = express.Router();
 
-  const Quiz = require('../models/quiz')(sequelize, Sequelize.DataTypes);
-  const Question = require('../models/question')(sequelize, Sequelize.DataTypes);
-  const Choice = require('../models/choice')(sequelize, Sequelize.DataTypes);
+    const Quiz = require('../models/quiz')(sequelize, Sequelize.DataTypes);
+    const Question = require('../models/question')(sequelize, Sequelize.DataTypes);
+    const Choice = require('../models/choice')(sequelize, Sequelize.DataTypes);
 
-  // quiz retrieval route
-  router.get('/', (req, res) => {
-    Quiz.findAll().then(quizzes => {
-      res.send(quizzes);
+    // quiz retrieval route
+    router.get('/', (req, res) => {
+        Quiz.findAll().then(quizzes => {
+            res.send(quizzes);
+        });
     });
-  });
 
-  router.get('/:quizID', function (req, res){
-      var obj = new Object();
+    router.get('/:quizID', function (req, res) {
+        var obj = new Object();
 
-      Quiz.findById(req.params.quizID).then(quiz => {
-            obj.quiz = quiz;
-          Question.findAll({where:{
-              quiz_id: quiz.id
-          }}).then(questions => {
-              quiz.questions = questions;
-              obj.quiz.questions = questions;
-              console.log(questions);
-              res.send(questions);
-              questions.forEach(function (question) {
-                  Choice.findAll({where:{
-                          question_id:question.id
-                      }}).then(choices =>{
-                          console.log(choices);
-
-                  })
-
-              })
+        Quiz.findById(req.params.quizID).then(quiz => {
+            quiz = {
+                id: quiz.id,
+                title: quiz.title
+            };
+            //obj.quiz = quiz;
+            // req.send(quiz);
+            Question.findAll({
+                where: {
+                    quiz_id: quiz.id
+                }
+            }).then(questions => {
 
 
+                //obj.questions = questions;
+                console.log(quiz);
+                //res.send(obj);
+
+                const promises = questions.map(function (question) {
+                    question = {
+                        id: question.id,
+                        text: question.text,
+                    };
+                    return Choice.findAll({
+                        where: {
+                            question_id: question.id
+                        }
+                    }).then(choices => {
+                        question.choices = choices;
+                        return question
+                    });
+                })
+
+                Promise.all(promises).then(questions => {
+                    quiz.questions = questions;
+
+                    res.send(quiz);
+                })
 
 
 
-
-
-          })
-
-      })
-
-
-  });
-  router.post('/', (req, res) => {
-    Quiz
-      .create({ title: req.body.title })
-      .then(createdQuiz => {
-        let promises = req.body.questions.map(question => {
-          return Question.create({ quiz_id: createdQuiz.id, text: question.text })
-            .then(createdQuestion => {
-              let choicePromises = question.choices.map(choice => {
-                return Choice.create({
-                  question_id: createdQuestion.id,
-                  text: choice.text,
-                  correct: choice.correct
-                });
-              });
-
-              return Promise.all(choicePromises);
             })
-        });
 
-        Promise.all(promises).then(createdChoices => {
-          res.send(createdQuiz);
-        });
-      });
-  });
+        })
 
-  return router;
+
+    });
+    router.post('/', (req, res) => {
+        Quiz
+            .create({title: req.body.title})
+            .then(createdQuiz => {
+                let promises = req.body.questions.map(question => {
+                    return Question.create({quiz_id: createdQuiz.id, text: question.text})
+                        .then(createdQuestion => {
+                            let choicePromises = question.choices.map(choice => {
+                                return Choice.create({
+                                    question_id: createdQuestion.id,
+                                    text: choice.text,
+                                    correct: choice.correct
+                                });
+                            });
+
+                            return Promise.all(choicePromises);
+                        })
+                });
+
+                Promise.all(promises).then(createdChoices => {
+                    res.send(createdQuiz);
+                });
+            });
+    });
+
+    return router;
 };
 
 module.exports = routesFunction;
