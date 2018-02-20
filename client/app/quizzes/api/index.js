a21e394b2a5d35efce44717235d83cb6442e2aa1
Imad Collin <Admin@student251-145.eduroam.uu.se>
2018-02-20 16:43:59 +0100

    Conect to mock server

--- client/app/quizzes/api/index.js
+++ client/app/quizzes/api/index.js
@@ -10,12 +10,17 @@ type QuizListResponse = Array<{
 type QuizCreateResponse = {
   id: number,
 };
+/*
+==================================================
+This link is for mock data :
+https://2eacf2d7-bcd6-4ca9-bb15-0933307fac6e.mock.pstmn.io/quizzes
+==================================================
+*/
 
 export const fetchQuizzes = (): Promise<QuizListResponse> =>
-  axios.get('/quizzes').then((response) => response.data);
-
+axios.get('https://2eacf2d7-bcd6-4ca9-bb15-0933307fac6e.mock.pstmn.io/quizzes').then((response) => response.data);
 export const storeQuiz = (quiz: Quiz): Promise<QuizCreateResponse> =>
   axios.post('/quizzes', quiz).then(response => response.data);
-
+// Get the quiz using with based on the specific ID
 export const fetchQuiz = (quizId: number): Promise<AnswerQuiz> =>
-  axios.get(`/quizzes/${quizId}`).then(response => response.data);
+  axios.get(`https://2eacf2d7-bcd6-4ca9-bb15-0933307fac6e.mock.pstmn.io/quizzes/${quizId}`).then(response => response.data);
