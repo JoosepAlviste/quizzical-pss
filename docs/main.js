1412d86a7712835678c4e375e0b9c40e5da6f83f
Joosep Alviste <joosep.alviste@gmail.com>
2018-02-24 21:38:21 +0100

    Site: New version download links

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,11 @@
 var primaryOS = '';
 
+var downloadLinks = [
+  'MacOS': 'https://github.com/JoosepAlviste/quizzical/releases/download/v0.2.0/Quizzical-0.2.0.dmg',
+  'Windows': 'https://github.com/JoosepAlviste/quizzical/releases/download/v0.2.0/Quizzical.Setup.0.2.0.exe',
+  'Linux': 'https://github.com/JoosepAlviste/quizzical/releases/download/v0.2.0/quizzical-0.2.0-x86_64.AppImage',
+];
+
 function getOS() {
   var os = 'Unknown OS';
   if (navigator.appVersion.indexOf('Win') != -1) {
@@ -24,9 +30,7 @@ function setDownloadLinks() {
   var others = [];
 
   var $primaryButton = document.getElementById('js-primary-os-button');
-  $primaryButton.innerHTML = '<b>Download</b> for <b>' + os + '</b>';
-
-  console.log($primaryButton, getDownloadPath(os));
+  $primaryButton.innerHTML = downloadLinkText(os);
   $primaryButton.href = getDownloadPath(os);
 
   if (os === 'Windows') {
@@ -38,19 +42,17 @@ function setDownloadLinks() {
   }
 
   document.querySelectorAll('.download__dropdown-item').forEach(function ($item, i) {
-    $item.innerHTML = '<b>Download</b> for <b>' + others[i] + '</b>';
+    $item.innerHTML = downloadLinkText(others[i]);
     $item.href = getDownloadPath(others[i]);
   });
 }
 
 function getDownloadPath(os) {
-  if (os === 'MacOS') {
-    return 'https://github.com/JoosepAlviste/quizzical/releases/download/0.1.0/Quizzical.dmg';
-  } else if (os === 'Windows') {
-    return 'https://github.com/JoosepAlviste/quizzical/releases/download/0.1.0/Quizzical.Setup.exe';
-  } else if (os === 'Linux') {
-    return 'https://github.com/JoosepAlviste/quizzical/releases/download/0.1.0/Quizzical.AppImage';
-  }
+  return downloadLinks[os];
+}
+
+function downloadLinkText(os) {
+  return '<b>Download</b> for <b>' + os + '</b>';
 }
 
 document.addEventListener('DOMContentLoaded', function () {
