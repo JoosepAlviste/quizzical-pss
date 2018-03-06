7d1c448fed4f35822e0d5b18566d68b25ca6e352
Imad Collin <Admin@MacBook.local>
2018-03-07 00:38:55 +0100

    Replace mock server with real

--- client/app/quizzes/api/index.js
+++ client/app/quizzes/api/index.js
@@ -18,12 +18,12 @@ https://2eacf2d7-bcd6-4ca9-bb15-0933307fac6e.mock.pstmn.io/quizzes
 */
 
 export const fetchQuizzes = (): Promise<QuizListResponse> =>
-axios.get('https://2eacf2d7-bcd6-4ca9-bb15-0933307fac6e.mock.pstmn.io/quizzes').then((response) => response.data);
+axios.get('http://104.131.43.24/quizzes').then((response) => response.data);
 export const storeQuiz = (quiz: Quiz): Promise<QuizCreateResponse> =>
   axios.post('/quizzes', quiz).then(response => response.data);
 // Get the quiz using with based on the specific ID
 export const fetchQuiz = (quizId: number): Promise<AnswerQuiz> =>
-  axios.get(`https://2eacf2d7-bcd6-4ca9-bb15-0933307fac6e.mock.pstmn.io/quizzes/${quizId}`).then(response => response.data);
+  axios.get(`http://104.131.43.24/quizzes/${quizId}`).then(response => response.data);
 
 
 /*
