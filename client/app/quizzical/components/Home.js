8408c94d53dabcba44339f6827754484f25b1f9d
Imad Collin <Admin@MacBook.local>
2018-03-07 01:03:45 +0100

    Beautify home view for presentation

--- client/app/quizzical/components/Home.js
+++ client/app/quizzical/components/Home.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import styles from './Home.css';
 import styles from '../quizzical-shared/shared-style.css';
+import Image from './1.jpg'
 
 type Props = {};
 
@@ -13,12 +14,15 @@ export default class Home extends Component<Props> {
     return (
       <div>
         <div className={styles.container} data-tid="container">
-          <h2>Home</h2><br />
+          <h2 style={{textAlign:'center', fontWeight:'bolder'}}>Take a Journey</h2><br />
 
-          <Link to="/counter">Counter</Link><br />
-          <Link to="/quizzes">Quizzes</Link>
-          <Link to="/reg">Registration</Link>
+          {/* <Link to="/counter">Counter</Link><br /> */}
+          <Link to="/quizzes"> <img src={Image}/></Link>
+          {/* <Link to="/reg">Registration</Link> */}
 
+          <h2 style={{textAlign:'center',fontStyle:'italic'}}>Rights to Platform spanning course </h2><br />
+          <h2 style={{textAlign:'center',fontStyle:'italic'}}>2018  Johan Sinder</h2><br />
+          <p style={{textAlign:'center',fontStyle:'italic' ,fontSize:'10px'}}>Image From https://portal.somersfield.bm/Regan/images/quiz1.jpg</p><br />
         </div>
 
       </div>
