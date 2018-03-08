3c4bc612741491302949ebecb817670e930945c3
root <Alexandros.Patsanis.4485@student.uu.se>
2018-03-08 15:08:51 +0000

    update travis file file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Copy files to api droplet...."
-rsync -r --delete-after --quitet $TRAVIS_BUILD_DIR root@159.65.226.145:quizzical
+rsync -r --delete-after --quitet $TRAVIS_BUILD_DIR root@159.65.226.145:quizzical/server
 
 echo "Copying Successful"
 echo "Restarting Server "
