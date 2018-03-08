fb0992c81f8dd17c0d808deb957758f9cf96e9fd
alexofficial <alexandros.patsanis.4485@student.uu.se>
2018-03-08 11:14:39 +0100

    update deploy file for check if integration works

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@ echo "Copy files to api droplet..."
 rsync -r --delete-after --quite $TRAVIS_BUILD_DIR root@159.65.226.145:quizzical
 
 echo "Copying Successful"
-echo "Restarting Server"
+echo "Restarting Server "
 
 ssh  root@159.65.226.145 "pm2 kill; cd quizzical/server; pm2 start server.js"
 
-echo "Server is running!"
+echo "Server is running Successfully!"
