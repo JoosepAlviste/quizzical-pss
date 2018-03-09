f94d10545a7639a151ff7c4e42ed84b9d02764d1
root <Alexandros.Patsanis.4485@student.uu.se>
2018-03-09 16:23:11 +0000

    deploy sh update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@159.65.226.145
 echo "Copying Successful"
 echo "Restarting Server "
 
-ssh  root@159.65.226.145 "pm2 kill && cd quizzical/server &&  pm2 start server.js"
+ssh  root@159.65.226.145 "pm2 kill; cd quizzical/server; pm2 start server.js"
 
 echo "Server is running Successfully!"
