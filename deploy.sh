64eae30ffba88524da5ef5e2a85788a4e6c30151
alexofficial <alexandros.patsanis.4485@student.uu.se>
2018-03-09 17:26:45 +0100

    updated deploy sh for travis

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ echo "Restarting Server "
 
 ssh  root@159.65.226.145 "pm2 kill; cd quizzical/server; pm2 start server.js"
 
-echo "Server is running Successfully!"
+echo "Server is running Successfully! Yeahh"
