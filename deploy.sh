777171e5c3d4abea2759ea9138a3a18bbd660c3d
root <Alexandros.Patsanis.4485@student.uu.se>
2018-03-09 15:18:03 +0000

    update travis yml and deploy sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 echo "Copy files to api droplet...."
  
-rsync -r --delete-after --quitet $TRAVIS_BUILD_DIR root@159.65.226.145:quizzical/server
-
+rsync -r --delete-after --quiet $TRAVIS_BUILD_DIR root@159.65.226.145:quizzical
 
 echo "Copying Successful"
 echo "Restarting Server "
 
-ssh  root@159.65.226.145 "pm2 kill && cd quizzical/server &&  pm2 start server.js"
+ssh  root@159.65.226.145 "pm2 kill; cd quizzical/server;  pm2 start server.js"
 
 echo "Server is running Successfully!"
